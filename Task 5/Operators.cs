17849b1aed1dd98c8dd7c29ee74113758d8a1e45
bibek0-0 <luitelbibek0@gmail.com>
2025-11-19 15:05:44 +0545

    task 5

---
+++ Task 5/Operators.cs
@@ -0,0 +1,12 @@
+﻿namespace Task_5
+{
+    internal class EnumAndRecord
+    {
+        public enum DayType
+        {
+            weekday,
+            weekend
+        }
+        public record Book(string title, string author, double price);
+    }
+}
