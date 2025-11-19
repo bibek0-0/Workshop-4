a14751a48212e85dbb4b49ab13199fe50ceb2352
bibek0-0 <luitelbibek0@gmail.com>
2025-11-19 14:27:56 +0545

    Task 3

---
+++ Task 3/Operators.cs
@@ -0,0 +1,24 @@
+﻿namespace Task_3
+{
+    internal class ParameterDemo
+    {
+        public void Increase(ref int number)
+        {
+            number = number + 10;
+        }
+        public void GetFullName(out string fullname)
+        {
+
+            fullname = "Bibek Luitel";
+        }
+        public int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int n in numbers)
+            {
+                sum = sum + n;
+            }
+            return sum;
+        }
+    }
+}
